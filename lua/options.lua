f657da075859fa29dfd17d71aedbb98df79d18c6
ancion <ancion@github.com>
2024-05-13 18:10:51 +0800

    window shell to pwsh

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,17 @@
 -- Default options that are always set: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/options.lua
 -- Add any additional options here
 
+if string.match(vim.loop.os_uname().sysname,"Windows") == "Windows" then
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellcmdflag =
+	  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.cmd([[
+		  let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		  let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		  set shellquote= shellxquote=
+    ]])
+end
+
 vim.opt.conceallevel = 2 -- enable conceal
 vim.opt.concealcursor = ""
 vim.opt.list = false -- show whitespace characters
