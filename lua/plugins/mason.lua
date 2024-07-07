49c7493b7bcd549a6b786d7aafc248a87656f63f
ancion <ancion@github.com>
2024-07-07 08:48:54 +0800

    delete mason custom-registry config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,6 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.registries = require("astrocore").list_insert_unique(opts.registries, {
-        "lua:custom-registry",
         "github:mason-org/mason-registry",
       })
       opts.ui = {
