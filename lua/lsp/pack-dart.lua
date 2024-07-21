6185c4cc6872f92660e5b9f6cc09e7cd702dd117
ancion <ancion@github.com>
2024-07-21 12:43:20 +0800

    add kotlin add dart lsp

---
+++ lua/lsp/pack-dart.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      handlers = { dartls = false },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart" })
+      end
+    end,
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    ft = "dart",
+    opts = function(_, opts)
+      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
+      if astrolsp_avail then opts.lsp = astrolsp.lsp_opts "dartls" end
+      opts.debugger = { enabled = true }
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    specs = {
+      -- Add "flutter" extension to "telescope"
+      {
+        "nvim-telescope/telescope.nvim",
+        optional = true,
+        opts = function() require("telescope").load_extension "flutter" end,
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart-debug-adapter" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart" })
+    end,
+  },
+}
---
+++ lua/lsp/pack-kotlin.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "kotlin" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "kotlin_language_server" })
+    end,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ktlint" })
+    end,
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "kotlin" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        { "kotlin-language-server", "ktlint", "kotlin-debug-adapter" }
+      )
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
+}
