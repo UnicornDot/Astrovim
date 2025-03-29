aa6cf9eab7a2366b36f62afdc80104996c7def39
ancion <ancion@github.com>
2025-03-29 12:49:35 +0800

    update avanta token takes

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -27,7 +27,8 @@ return {
         __inherited_from = "openai",
         api_key_name = "DEEPSEEK_API_KEY",
         endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder"
+        model = "deepseek-coder",
+        max_tokens = 1000
       },
     },
     file_selector = {
