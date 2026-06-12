6031bf7fb1a84acff62bc69d0b3f12ee7598e948
ancion <ancion@github.com>
2026-06-13 07:37:45 +0800

    java lsp runtime list config

--- lua/lsp/pack-java.lua
+++ lua/lsp/pack-java.lua
@@ -11,6 +11,35 @@ return {
       opts = function(_, opts)
         local astrocore  = require("astrocore")
         opts.servers = astrocore.list_insert_unique(opts.servers or {}, { "jdtls" })
+        opts.config = vim.tbl_deep_extend("keep", opts.config or {}, {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-11",
+                      path = "~/.jdks/corretto-11.0.31",
+                    },
+                    {
+                      name = "JavaSE-17",
+                      path = "~/.jdks/corretto-17.0.19",
+                      default = true,
+                    },
+                    {
+                      name = "JavaSE-21",
+                      path = "~/.jdks/corretto-21.0.10",
+                    },
+                    {
+                      name = "JavaSE-25",
+                      path = "~/.jdks/corretto-25.0.1",
+                    }
+                  }
+                }
+              }
+            }
+          }
+        })
         opts.handlers = astrocore.extend_tbl(opts.handlers or {}, {
           jdtls = function(server)
             require("lazy").load { plugins = { "nvim-java" } }
